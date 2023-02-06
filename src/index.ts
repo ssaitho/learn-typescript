2df3626ab5a76c2333c49f2557cd675be793cc22
s-saito <s.s.saito108@gmail.com>
2023-02-06 19:23:50 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const message: string = "hello, world!";
-console.log(message);
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("数値を入力してください:", (line) => {
+  const num = Number(line);
+  console.log(`${num + 1000}が入力されました`);
+  rl.close();
+});
