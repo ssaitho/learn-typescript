155ec18c1264c668a0d0433a3f85d65f36d2e9c3
s-saito <s.s.saito108@gmail.com>
2023-02-09 18:44:09 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,33 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-rl.question("数値を入力してください:", (line) => {
-  const num = Number(line);
-  if (0 <= num && num < 100) {
-    console.log(`${num}は0以上100未満です`);
-  } else {
-    console.log(`${num}は0以上100未満ではありません`);
+// rl.question("数値を入力してください:", (line) => {
+//   const num = Number(line);
+//   if (0 <= num && num < 100) {
+//     console.log(`${num}は0以上100未満です`);
+//   } else {
+//     console.log(`${num}は0以上100未満ではありません`);
+//   }
+//   rl.close();
+// });
+
+// rl.question("名前を入力してください:", (name) => {
+//   const displayName = name || "名無し";
+//   console.log(`こんにちは、${displayName}さん`);
+//   rl.close();
+// });
+
+rl.question("コマンドを入力してください:", (name) => {
+  switch (name) {
+    case "greet":
+      console.log("こんにちは。");
+      break;
+    case "cat":
+      console.log("あなたは猫派ですか？");
+      console.log("私は犬派です。");
+      break;
+    default:
+      console.log(`コマンド「${name}」を認識できませんでした。`);
   }
   rl.close();
 });
