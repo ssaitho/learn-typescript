f2551047fbdf9cb07497e0deaddbb1d326638348
s-saito <s.s.saito108@gmail.com>
2023-02-19 19:51:30 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,22 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const messages = {
+  good: "0以上の数値が入力されました!",
+  bad: "負の数値を入力しないでください!",
+};
+
+rl.question("数値を入力してください:", (line) => {
+  const num = Number(line);
+  console.log(messages[num >= 0 ? "good" : "bad"]);
+  rl.close;
+});
+
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
 // rl.question("数値を入力してください:", (line) => {
 //   const num = Number(line);
 //   if (0 <= num && num < 100) {
@@ -21,17 +37,33 @@ const rl = createInterface({
 //   rl.close();
 // });
 
-rl.question("コマンドを入力してください:", (name) => {
-  switch (name) {
-    case "greet":
-      console.log("こんにちは。");
-      break;
-    case "cat":
-      console.log("あなたは猫派ですか？");
-      console.log("私は犬派です。");
-      break;
-    default:
-      console.log(`コマンド「${name}」を認識できませんでした。`);
-  }
-  rl.close();
-});
+// rl.question("コマンドを入力してください:", (name) => {
+//   switch (name) {
+//     case "greet":
+//       console.log("こんにちは。");
+//       break;
+//     case "cat":
+//       console.log("あなたは猫派ですか？");
+//       console.log("私は犬派です。");
+//       break;
+//     default:
+//       console.log(`コマンド「${name}」を認識できませんでした。`);
+//   }
+//   rl.close();
+// });
+// let result = "";
+// for (let i = 1; i <= 100; i++) {
+//   if (i > 1) {
+//     result += " ";
+//   }
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     result += "FizzBuzz";
+//   } else if (i % 3 === 0) {
+//     result += "Fizz";
+//   } else if (i % 5 === 0) {
+//     result += "Buzz";
+//   } else {
+//     result += String(i);
+//   }
+// }
+// console.log(result);
